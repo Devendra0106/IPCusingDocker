e6866988e05307fb583394c4098a90ad2c1d03fa
gadekalp <69337649+gadekalp@users.noreply.github.com>
2020-08-08 12:47:46 +0530

    Adding main python file producer

---
+++ Producer/producer.py
@@ -0,0 +1,23 @@
+import socket
+import random
+import time
+def producer():
+	
+	host = socket.gethostbyname('ipc_server_dns_name')
+	port = 5000
+	prod_socket = socket.socket()
+	prod_socket.bind((host,port))
+	prod_socket.listen(3)
+	conn, address = prod_socket.accept()
+	print("Connection from: "+str(address))
+	
+	while True:
+		data = str(random.randint(1,24))
+		print('Sent: ' +data)
+		conn.send(data.encode())
+		#time.sleep(3)
+		#i-=1
+	conn.close()
+	
+if __name__ == '__main__':
+	producer()
